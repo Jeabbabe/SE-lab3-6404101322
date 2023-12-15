5437c989af1db630e71ca0e72d6721c04511380e
Jeabbabe <babe-ja@hotmail.com>
2023-12-15 10:16:27 +0700

    Change to thai lang

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,29 +9,29 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อ-สกุล *"
+        hint="ใส่ชื่อและนามสกุล"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'กรุณาใส่ชื่อและนามสกุล']"
       />
 
       <q-input
         filled
         type="number"
         v-model="age"
-        label="Your age *"
+        label="อายุ *"
         lazy-rules
         :rules="[
-          val => val !== null && val !== '' || 'Please type your age',
-          val => val > 0 && val < 100 || 'Please type a real age'
+          val => val !== null && val !== '' || 'ใส่อายุ',
+          val => val > 0 && val < 100 || 'กรุณาใส่อายุจริง'
         ]"
       />
 
-      <q-toggle v-model="accept" label="I accept the license and terms" />
+      <q-toggle v-model="accept" label="ฉันยอมรับเงื่อนไขและข้อกำหนด" />
 
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="ส่ง" type="submit" color="primary"/>
+        <q-btn label="ล้าง" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
 
